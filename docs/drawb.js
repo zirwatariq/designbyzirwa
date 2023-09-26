bdd30e7b1e6353e1fc29c2b8189aa382704c9c88
zirwa <ztariq@oswego.edu>
2023-09-25 23:27:30 -0400

    navigation push

--- docs/drawb.js
+++ docs/drawb.js
@@ -1,3 +1,4 @@
+/*
 // Get a reference to the image link element
 const backToTopLink = document.querySelector('.backtopimage a');
 
@@ -12,3 +13,20 @@ function handleKeyboardEvents(event) {
 
 // Add a keyboard event listener to the link
 backToTopLink.addEventListener('keydown', handleKeyboardEvents);
+*/
+       /* place the navigation on the page*/ 
+
+	   function openNav() {
+		var w = window.innerWidth;
+		 /*var rem_font =  window.getComputedStyle(document.getElementById("page")).fontSize;
+        var remW = w / Number(rem_font);*/
+		if (w < 1120){
+			document.getElementById("theDrawer").style.right = "0";
+		}
+		}
+ /*place the navigation off the page */
+	function closeNav() {
+		document.getElementById("theDrawer").style.right = "-250px";
+	}
+
+	
